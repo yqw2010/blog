2f719f70e64eba96219291bf8c05af578540c231
阎王 <yanwang.lj@alibaba-inc.com>
2015-09-06 16:25:06 +0800

    backup

--- blog/themes/hexo-theme-moustache/source/public/js/main.js
+++ blog/themes/hexo-theme-moustache/source/public/js/main.js
@@ -631,4 +631,12 @@ $(window).on("load", function(){
             $("#followMeOnWeibo").html('<wb:follow-button uid="1812166904" type="red_1" width="67" height="24" style="vertical-align:middle;display:inline-block" ></wb:follow-button>');
         });
     }
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-67248043-1', 'auto');
+    ga('send', 'pageview');
 });
